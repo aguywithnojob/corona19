b5527059ecf08da192518ebac00574ab84147830
Gaut9a8m <gaut9a8m@gmail.com>
2020-03-26 13:17:33 +0530

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -73,7 +73,7 @@ export class AppComponent implements OnInit {
         }
         else{
           this.statewise_stat.push(item)
-          console.log(this.statewise_stat)
+          // console.log(this.statewise_stat)
         }
     });
     
